4e34d599382b6998c83527b0074a7196e765d9ce
Gida Pataki <gida.pataki@prezi.com>
2017-03-29 21:02:26 +0200

    Solve in 87 chars

--- qualif/iskolabusz/solution.cpp
+++ qualif/iskolabusz/solution.cpp
@@ -1,16 +1,8 @@
 #include <cstdio>
 
 int main() {
-    int i = 1, d = 1, y, s;
-    scanf("%d", &s);
-    y = s;
-
-    while (s > 1) {
-        i += d;
-        d *= 2;
-        if (s % 2) y = i, i += d;
-        s /= 2;
-    }
-
-    printf("%d\n", y);
+    int h = 1, n;
+    scanf("%d", &n);
+    while (n > h) h *= 2;
+    printf("%d\n", 2 * n - h);
 }
