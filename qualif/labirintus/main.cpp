6271f2115894e39a4343287006e24e6667374f34
Andras Kucsma <andras.kucsma@prezi.com>
2017-03-30 00:59:33 +0200

    Input reading + GetAdjacent()

--- qualif/labirintus/main.cpp
+++ qualif/labirintus/main.cpp
@@ -6,9 +6,26 @@ enum class Field {
     kWall, kFree, kScreen
 };
 
+struct Point {
+    Point() = default;
+    Point(int x, int y) : x(x), y(y) {}
+    int x = -1;
+    int y = -1;
+};
+
+std::ostream& operator<<(std::ostream& os, const Point& p) {
+    os << "(" << p.x << ", " << p.y << ")";
+    return os;
+}
+
 class HexaMatrix {
 public:
     HexaMatrix(std::istream& in);
+
+    bool IsValid(const Point& p) const;
+    Field GetFieldAt(const Point& p) const;
+
+    std::vector<Point> GetAdjacents(const Point& p) const;
 private:
     std::vector<std::vector<Field>> grid;
 };
@@ -36,6 +53,56 @@ HexaMatrix::HexaMatrix(std::istream& in) {
     }
 }
 
+bool HexaMatrix::IsValid(const Point& p) const {
+    return
+        p.x >= 0 && p.y >= 0 &&
+        p.y < int(grid.size()) && p.x < int(grid[0].size());
+}
+
+std::vector<Point> HexaMatrix::GetAdjacents(const Point& p) const {
+    int w = grid[0].size();
+
+    std::vector<Point> result;
+    result.reserve(6);
+
+    result.push_back({p.x, p.y - 1});
+    result.push_back({p.x, p.y + 1});
+
+    if (p.x < w/2) {
+        int x_before = p.x + w/2 - 1;
+        int x_after = p.x + w/2;
+
+        result.push_back({x_before, p.y});
+        result.push_back({x_before, p.y - 1});
+
+        result.push_back({x_after, p.y});
+        result.push_back({x_after, p.y - 1});
+
+    } else {
+        int x_before = p.x - w/2;
+        int x_after = p.x - w/2 + 1;
+
+        result.push_back({x_before, p.y});
+        result.push_back({x_before, p.y + 1});
+
+        result.push_back({x_after, p.y});
+        result.push_back({x_after, p.y + 1});
+    }
+
+    return result;
+}
+
+Field HexaMatrix::GetFieldAt(const Point& p) const {
+    assert(IsValid(p));
+    return grid[p.y][p.x];
+}
+
 int main() {
     HexaMatrix map(std::cin);
+
+    auto adj = map.GetAdjacents(Point{2, 2});
+
+    for (auto p : adj) {
+        std::cout << p << std::endl;
+    }
 }
