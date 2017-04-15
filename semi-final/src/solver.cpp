d9c85449b26b872481b537fa453db490ae064f73
Andras Kucsma <andras.kucsma@prezi.com>
2017-04-15 23:07:26 +0200

    Input reading

--- semi-final/src/solver.cpp
+++ semi-final/src/solver.cpp
@@ -6,12 +6,12 @@
 void solver::init(const std::vector<std::string>& field_infos) {
 	std::cerr << "We got these field informations:" << std::endl;
 
-	for (auto& field : field_infos) {
-		std::cerr << field << std::endl;
+	for (auto& info : field_infos) {
+		std::cerr << info << std::endl;
 	}
 
-	for (auto& field : field_infos) {
-		std::stringstream ss(field);
+	for (auto& info : field_infos) {
+		std::stringstream ss(info);
 		std::string command;
 		ss >> command;
 		if (command == "MESSAGE") {
@@ -20,8 +20,13 @@ void solver::init(const std::vector<std::string>& field_infos) {
 			ss >> level_;
 		} else if (command == "SIZE") {
 			ss >> width_ >> height_;
+			tick.field.resize(width_);
+			for (auto& col : tick.field) {
+				col.resize(height_);
+			}
 		} else if (command == "DISPLAYS") {
 			ss >> display_count_;
+			tick.displays.resize(display_count_);
 		} else if (command == "PLAYER") {
 			ss >> player_index_;
 		} else if (command == "MAXTICK") {
@@ -35,13 +40,45 @@ void solver::init(const std::vector<std::string>& field_infos) {
 std::vector<std::string> solver::process(const std::vector<std::string>& tick_infos) {
 	std::cerr << "We got these tick informations:" << std::endl;
 
-	for(std::size_t i = 0; i < tick_infos.size(); ++i) {
-		std::cerr << tick_infos[i] << std::endl;
+	for (auto& info : tick_infos) {
+		std::cerr << info << std::endl;
 	}
 
+	for (auto& info : tick_infos) {
+		std::stringstream ss(info);
+		std::string command;
+		ss >> command;
+		if (command == "MESSAGE") {
+			// nothing to do with this
+		} else if (command == "TICK") {
+			ss >> tick.tick;
+		} else if (command == "FIELDS") {
+			for (int y = 0; y < height_; ++y) {
+				for (int x = 0; y < width_; ++x) {
+					ss >> tick.field[x][y];
+				}
+			}
+		} else if (command == "DISPLAY") {
+			int index;
+			ss >> index;
+			ss >> tick.displays[index].x >> tick.displays[index].y;
+		} else if (command == "PLAYER") {
+			ss >> tick.current_player;
+		} else if (command == "TARGET") {
+			ss >> tick.target_display;
+		} else if (command == "EXTRAFIELD") {
+			ss >> tick.our_field_type;
+		}
+	}
+
+	if (player_index_ != tick.current_player) {
+		// not our turn
+		return {};
+	}
+
+	// our turn
 	std::vector<std::string> result;
-	// TODO ...
-	// send an empty vector if not your turns
+	// TODO
 	return result;
 }
 
--- semi-final/src/solver.h
+++ semi-final/src/solver.h
@@ -4,6 +4,13 @@
 #include <vector>
 #include <string>
 
+struct Point {
+	Point() = default;
+	Point(int x, int y) : x(x), y(y) {}
+	int x = -1;
+	int y = -1;
+};
+
 class solver {
 public:
 	void init(const std::vector<std::string>& field_infos);
@@ -19,6 +26,16 @@ private:
 	int display_count_ = -1;
 	int player_index_ = -1;
 	int max_tick_ = -1;
+
+	// stuff from process()
+	struct {
+		int tick = -1;
+		int current_player = -1;
+		int target_display = -1;
+		int our_field_type = -1;
+		std::vector<std::vector<int>> field;
+		std::vector<Point> displays;
+	} tick;
 };
 
 #endif // SOLVER_H_INCLUDED
