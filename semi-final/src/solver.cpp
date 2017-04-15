2d91d7cdeb78fbf243185fa46d92cc51e1b4cd0b
Andras Kucsma <andras.kucsma@prezi.com>
2017-04-15 22:17:47 +0200

    Read init input

--- semi-final/src/solver.cpp
+++ semi-final/src/solver.cpp
@@ -1,12 +1,34 @@
 #include "solver.h"
 
 #include <iostream>
+#include <sstream>
 
 void solver::init(const std::vector<std::string>& field_infos) {
 	std::cerr << "We got these field informations:" << std::endl;
 
-	for(std::size_t i = 0; i < field_infos.size(); ++i) {
-		std::cerr << field_infos[i] << std::endl;
+	for (auto& field : field_infos) {
+		std::cerr << field << std::endl;
+	}
+
+	for (auto& field : field_infos) {
+		std::stringstream ss(field);
+		std::string command;
+		ss >> command;
+		if (command == "MESSAGE") {
+			// nothing to do with this
+		} else if (command == "LEVEL") {
+			ss >> level_;
+		} else if (command == "SIZE") {
+			ss >> width_ >> height_;
+		} else if (command == "DISPLAYS") {
+			ss >> display_count_;
+		} else if (command == "PLAYER") {
+			ss >> player_index_;
+		} else if (command == "MAXTICK") {
+			ss >> max_tick_;
+		} else {
+			std::cerr << "WARNING: unhandled command: " << command << std::endl;
+		}
 	}
 }
 
--- semi-final/src/solver.h
+++ semi-final/src/solver.h
@@ -11,6 +11,14 @@ public:
 	std::vector<std::string> process(const std::vector<std::string>& tick_infos);
 
 	void end(const std::string& message);
+private:
+	// stuff we get form init()
+	int level_ = -1;
+	int width_ = -1; // N
+	int height_ = -1; // M
+	int display_count_ = -1;
+	int player_index_ = -1;
+	int max_tick_ = -1;
 };
 
 #endif // SOLVER_H_INCLUDED
