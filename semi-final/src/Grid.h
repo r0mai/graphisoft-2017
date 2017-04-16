443305979e8cc0e2d8ae4557ad9f8489a4e20c1d
Gida Pataki <gida.pataki@prezi.com>
2017-04-16 17:07:50 +0200

    Move impl to grid.cpp

--- semi-final/src/Grid.h
+++ semi-final/src/Grid.h
@@ -9,45 +9,22 @@
 
 class Grid {
 public:
-    int Width() const { return fields_.Width(); }
-    int Height() const { return fields_.Height();; }
-    Point Size() const { return {fields_.Width(), fields_.Height()}; }
+    int Width() const;
+    int Height() const;
+    Point Size() const;
 
-    void Init(int width, int height, int displays, int players) {
-		fields_ = Matrix<int>(width, height, 0);
-        display_.resize(displays, {-1, -1});
-        position_.resize(players, {-1, -1});
-    }
-
-    void Randomize() {
-		for (int x = 0; x < fields_.Width(); ++x) {
-			for (int y = 0; y < fields_.Height(); ++y) {
-				fields_.At(x, y) = 1 + (rand() % 15);
-			}
-		}
-    }
-
-    void UpdateFields(std::vector<int> fields) {
-		fields_.SetFields(std::move(fields));
-    }
-
-    void UpdateDisplay(int index, const Point& pos) {
-        display_[index] = pos;
-    }
-
-    void UpdatePosition(int player, const Point& pos) {
-        position_[player] = pos;
-    }
-
-    int At(int x, int y) const {
-		return fields_.At(x, y);
-    }
-
+    void Init(int width, int height, int displays, int players);
+    void Randomize();
+    void UpdateFields(std::vector<int> fields);
+    void UpdateDisplay(int index, const Point& pos);
+    void UpdatePosition(int player, const Point& pos);
+    int At(int x, int y) const;
+    int Push(const Point& pos, int t);
 
 private:
     int players_ = -1;
     std::vector<Point> display_;
     std::vector<Point> position_;
-	Matrix<int> fields_;
+    Matrix<int> fields_;
 };
 
