737a87aebad0502e65d398d89d2761bada963aca
Andras Kucsma <andras.kucsma@prezi.com>
2017-04-16 19:14:31 +0200

    Test on worst case map

--- semi-final/src/test.cpp
+++ semi-final/src/test.cpp
@@ -3,13 +3,22 @@
 #include <iostream>
 #include <chrono>
 
+Matrix<int> WorstCaseMap(int w, int h) {
+	Matrix<int> field(15, 15, 10);
+	for (int y = 0; y < h; ++y) {
+		field.At(0,   y) = (y % 2 == 0) ? 12 : 9;
+		field.At(w-1, y) = (y % 2 == 0) ? 3 : 6;
+	}
+	return field;
+}
+
 int TestFloodFillTime() {
 	int sum = 0;
 	auto start = std::chrono::steady_clock::now();
 	for (int i = 0; i < 1000; ++i) {
-		Matrix<int> field(15, 15, 15); // worst case map for flood fill
+		auto field = WorstCaseMap(15, 15);
 		auto result = FloodFill(field, {rand() % 15, rand() % 15});
-		sum += result.At({rand() % 15, rand() % 15}) << '\n';
+		sum += result.At({rand() % 15, rand() % 15});
 	}
 	auto end = std::chrono::steady_clock::now();
 
@@ -21,5 +30,5 @@ int TestFloodFillTime() {
 }
 
 int main() {
-	TestFloodFillTime();
+	std::cout << TestFloodFillTime() << std::endl;
 }
