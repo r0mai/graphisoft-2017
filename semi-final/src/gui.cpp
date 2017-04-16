709fae6c546f99711fb689f4cd711c7f42bb2edf
Gida Pataki <gida.pataki@prezi.com>
2017-04-16 02:37:08 +0200

    Add initial gui

---
+++ semi-final/src/gui.cpp
@@ -0,0 +1,215 @@
+#include <cassert>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <cstdlib>
+#include <SFML/Graphics.hpp>
+#include "solver.h"
+
+
+class Grid {
+public:
+
+    int GetWidth() const { return width_; }
+    int GetHeight() const { return height_; }
+    Point GetSize() const { return {width_, height_}; }
+
+    void Init(int width, int height, int displays, int players) {
+        width_ = width;
+        height_ = height;
+        display_.resize(displays, {-1, -1});
+        position_.resize(players, {-1, -1});
+        fields_.resize(width * height, 0);
+    }
+
+    void Randomize() {
+        for (auto& v : fields_) {
+            v = 1 + (rand() % 15);
+        }
+    }
+
+    void UpdateFields(std::vector<int> fields) {
+        fields_ = std::move(fields);
+    }
+
+    void UpdateDisplay(int index, const Point& pos) {
+        display_[index] = pos;
+    }
+
+    void UpdatePosition(int player, const Point& pos) {
+        position_[player] = pos;
+    }
+
+    int At(int x, int y) const {
+        assert(x >= 0 && y >= 0 && x < width_ && y < height_);
+        return fields_[x + y * width_];
+    }
+
+private:
+    int width_ = -1;
+    int height_ = -1;
+    int players_ = -1;
+    std::vector<int> fields_;
+    std::vector<Point> display_;
+    std::vector<Point> position_;
+};
+
+
+
+struct App {
+    sf::RenderWindow window;
+    sf::Font font;
+    Grid grid;
+};
+
+
+void AdjustView(App& app) {
+    auto size = app.window.getSize();
+    float h = app.grid.GetHeight() + 2;
+    float w = app.grid.GetWidth() + 2;
+
+    if (size.x * h > size.y * w) {
+        w = size.x * h / size.y;
+    } else {
+        h = size.y * w / size.x;
+    }
+
+    app.window.setView(sf::View(sf::FloatRect(-1, -1, w, h)));
+}
+
+
+void HandleEvents(App& app) {
+    sf::Event event;
+    while (app.window.pollEvent(event)) {
+        switch (event.type) {
+            case sf::Event::Closed:
+                app.window.close();
+                break;
+            case sf::Event::Resized: {
+                AdjustView(app);
+                break;
+            }
+            default:
+                break;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    }
+}
+
+sf::RectangleShape CreateTile(const sf::Vector2f& pos) {
+    float size = 0.98f;
+    sf::RectangleShape shape(sf::Vector2f{size, size});
+    shape.setPosition(pos);
+    return shape;
+}
+
+sf::ConvexShape CreateRoute(const sf::Vector2f& pos, int tile) {
+    float pad = 0.3f;
+    float size = 0.98f;
+
+    sf::Vector2f dx(size, 0.f);
+    sf::Vector2f dy(0.f, size);
+
+    auto dx0 = dx * 0.f;
+    auto dx1 = dx * 1.f;
+    auto dxm0 = dx * pad;
+    auto dxm1 = dx * (1.f - pad);
+
+    auto dy0 = dy * 0.f;
+    auto dy1 = dy * 1.f;
+    auto dym0 = dy * pad;
+    auto dym1 = dy * (1.f - pad);
+
+    int count = 0;
+    count += !!(tile & 0x1);
+    count += !!(tile & 0x2);
+    count += !!(tile & 0x4);
+    count += !!(tile & 0x8);
+
+    sf::ConvexShape shape;
+    shape.setPointCount(count * 2 + 4);
+
+    int k = 0;
+    shape.setPoint(k++, dxm1 + dym0);
+    if (!!(tile & 0x1)) {
+        shape.setPoint(k++, dxm1 + dy0);
+        shape.setPoint(k++, dxm0 + dy0);
+    }
+
+    shape.setPoint(k++, dxm0 + dym0);
+    if (!!(tile & 0x2)) {
+        shape.setPoint(k++, dx0 + dym0);
+        shape.setPoint(k++, dx0 + dym1);
+    }
+
+    shape.setPoint(k++, dxm0 + dym1);
+    if (!!(tile & 0x4)) {
+        shape.setPoint(k++, dxm0 + dy1);
+        shape.setPoint(k++, dxm1 + dy1);
+    }
+
+    shape.setPoint(k++, dxm1 + dym1);
+    if (!!(tile & 0x8)) {
+        shape.setPoint(k++, dx1 + dym1);
+        shape.setPoint(k++, dx1 + dym0);
+    }
+    shape.setPosition(pos);
+
+    return shape;
+}
+
+
+void Draw(App& app) {
+    auto& window = app.window;
+    auto size = app.grid.GetSize();
+
+    window.clear();
+
+    for (int y = 0; y < size.y; ++y) {
+        for (int x = 0; x < size.x; ++x) {
+            auto tile = CreateTile(sf::Vector2f(x, y));
+            auto route = CreateRoute(sf::Vector2f(x, y), app.grid.At(x, y));
+            tile.setOutlineThickness(0.01);
+            tile.setOutlineColor(sf::Color(0x40, 0x30, 0x40));
+            tile.setFillColor(sf::Color(0x66, 0x50, 0x66));
+            route.setFillColor(sf::Color(0xf0, 0xf0, 0xe0));
+
+            window.draw(tile);
+            window.draw(route);
+        }
+    }
+
+    window.display();
+}
+
+
+void Run(App& app) {
+    while (app.window.isOpen()) {
+        HandleEvents(app);
+        Draw(app);
+    }
+}
+
+
+int main() {
+    App app;
+    app.grid.Init(14, 8, 0, 0);
+    app.grid.Randomize();
+
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+
+    app.window.create(
+        sf::VideoMode(1600, 1000), "Labirintus",
+        sf::Style::Default,
+        settings
+    );
+
+    AdjustView(app);
+    Run(app);
+
+    return 0;
+}
