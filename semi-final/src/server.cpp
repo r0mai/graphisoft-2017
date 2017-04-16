cba08b8d92021746b0639847b19f1dcb451cd35e
Matthias Vegh <matyas.vegh@gmail.com>
2017-04-16 20:47:55 +0200

    Refactor command parsing in server

--- semi-final/src/server.cpp
+++ semi-final/src/server.cpp
@@ -19,86 +19,74 @@ enum class Command {
 	Tick, Fields, End, Display, Position, Target, ExtraField
 };
 
+namespace detail {
+
+std::map<std::string, Command> getTokenToCommand() {
+	std::map<std::string, Command> result;
+	result.emplace("LOGIN", Command::Login);
+	result.emplace("PUSH", Command::Push);
+	result.emplace(".", Command::Over);
+	result.emplace("LEVEL", Command::Level);
+	result.emplace("MESSAGE", Command::Message);
+	result.emplace("SIZE", Command::Size);
+	result.emplace("DISPLAYS", Command::Displays);
+	result.emplace("PLAYER", Command::Player);
+	result.emplace("MAXTICK", Command::MaxTick);
+	result.emplace("TICK", Command::Tick);
+	result.emplace("FIELDS", Command::Fields);
+	result.emplace("END", Command::End);
+	result.emplace("DISPLAY", Command::Display);
+	result.emplace("POSITION", Command::Position);
+	result.emplace("TARGET", Command::Target);
+	result.emplace("EXTRAFIELD", Command::ExtraField);
+
+	return result;
+}
+
+std::map<Command, std::string> getCommandToToken() {
+	std::map<Command, std::string> result;
+	result.emplace(Command::Login, "LOGIN");
+	result.emplace(Command::Push, "PUSH");
+	result.emplace(Command::Over, ".");
+	result.emplace(Command::Level, "LEVEL");
+	result.emplace(Command::Message, "MESSAGE");
+	result.emplace(Command::Size, "SIZE");
+	result.emplace(Command::Displays, "DISPLAYS");
+	result.emplace(Command::Player, "PLAYER");
+	result.emplace(Command::MaxTick, "MAXTICK");
+	result.emplace(Command::Tick, "TICK");
+	result.emplace(Command::Fields, "FIELDS");
+	result.emplace(Command::End, "END");
+	result.emplace(Command::Display, "DISPLAY");
+	result.emplace(Command::Position, "POSITION");
+	result.emplace(Command::Target, "TARGET");
+	result.emplace(Command::ExtraField, "EXTRAFIELD");
+	return result;
+}
+
+} // namespace detail
+
 std::istream& operator>>(std::istream& is, Command& command) {
 	std::string token;
 	is >> token;
-	if (token == "LOGIN") {
-		command = Command::Login;
-	} else if (token == "PUSH") {
-		command = Command::Push;
-	} else if (token == ".") {
-		command = Command::Over;
-	} else if (token == "LEVEL") {
-		command = Command::Level;
-	} else if (token == "MESSAGE") {
-		command = Command::Message;
-	} else if (token == "SIZE") {
-		command = Command::Size;
-	} else if (token == "DISPLAYS") {
-		command = Command::Displays;
-	} else if (token == "PLAYER") {
-		command = Command::Player;
-	} else if (token == "MAXTICK") {
-		command = Command::MaxTick;
-	} else if (token == "TICK") {
-		command = Command::Tick;
-	} else if (token == "FIELDS") {
-		command = Command::Fields;
-	} else if (token == "END") {
-		command = Command::End;
-	} else if (token == "DISPLAY") {
-		command = Command::Display;
-	} else if (token == "POSITION") {
-		command = Command::Position;
-	} else if (token == "TARGET") {
-		command = Command::Target;
-	} else if (token == "EXTRAFIELD") {
-		command = Command::ExtraField;
-	} else {
-		std::cerr << "Failed parse: '" << token << "'" << std::endl;
-		throw std::runtime_error("Could not parse input");
+
+	static const auto tokenToCommand = detail::getTokenToCommand();
+
+	auto it = tokenToCommand.find(token);
+	if (it != tokenToCommand.end()) {
+		command = it->second;
+		return is;
 	}
-	return is;
+	std::cerr << "Failed parse: '" << token << "'" << std::endl;
+	throw std::runtime_error("Could not parse input");
 }
 
 
 std::ostream& operator<<(std::ostream& os, const Command& command) {
-	switch (command) {
-		case Command::Login:
-			return os << "LOGIN";
-		case Command::Push:
-			return os << "PUSH";
-		case Command::Goto:
-			return os << "GOTO";
-		case Command::Over:
-			return os << ".";
-		case Command::Level:
-			return os << "LEVEL";
-		case Command::Message:
-			return os << "MESSAGE";
-		case Command::Size:
-			return os << "SIZE";
-		case Command::Displays:
-			return os << "DISPLAYS";
-		case Command::Player:
-			return os << "PLAYER";
-		case Command::MaxTick:
-			return os << "MAXTICK";
-		case Command::Tick:
-			return os << "TICK";
-		case Command::Fields:
-			return os << "FIELDS";
-		case Command::End:
-			return os << "END";
-		case Command::Display:
-			return os << "DISPLAY";
-		case Command::Position:
-			return os << "POSITION";
-		case Command::Target:
-			return os << "TARGET";
-		case Command::ExtraField:
-			return os << "EXTRAFIELD";
-	}
+	static const auto commandToToken = detail::getCommandToToken();
+	auto it = commandToToken.find(command);
+	assert(it != commandToToken.end());
+	return os << it->second;
 }
 
 
