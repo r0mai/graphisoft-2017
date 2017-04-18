686816ac6d4004eba6a35f101df6b17f3587256e
Andras Kucsma <andras.kucsma@prezi.com>
2017-04-18 23:52:29 +0200

    Add FullFloodFill

--- semi-final/src/FloodFill.cpp
+++ semi-final/src/FloodFill.cpp
@@ -5,21 +5,38 @@
 
 #include "Util.h"
 
-Matrix<int> FloodFill(const Matrix<Field>& fields, const Point& origin) {
+Matrix<int> FloodFill(
+	const Matrix<Field>& fields,
+	const Point& origin)
+{
+	auto width = fields.Width();
+	auto height = fields.Height();
+
+	Matrix<int> reachable{width, height, 0};
+
+	FloodFillTo(reachable, fields, origin, 1);
+
+	return reachable;
+}
+
+void FloodFillTo(
+	Matrix<int>& fill_matrix,
+	const Matrix<Field>& fields,
+	const Point& origin,
+	int fill_value)
+{
 	std::stack<Point> stack;
 	stack.push(origin);
 	auto width = fields.Width();
 	auto height = fields.Height();
 
-	Matrix<int> reachable{width, height, false};
-
 	while (!stack.empty()) {
 		Point p = stack.top();
 		stack.pop();
-		if (reachable.At(p)) {
+		if (fill_matrix.At(p) == fill_value) {
 			continue;
 		}
-		reachable.At(p) = true;
+		fill_matrix.At(p) = fill_value;
 
 		if (p.x + 1 < width &&
 			IsEastOpen(fields.At(p)) &&
@@ -46,5 +63,21 @@ Matrix<int> FloodFill(const Matrix<Field>& fields, const Point& origin) {
 			stack.push({p.x, p.y - 1});
 		}
 	}
-	return reachable;
+}
+
+Matrix<int> FullFloodFill(const Matrix<Field>& fields) {
+	auto width = fields.Width();
+	auto height = fields.Height();
+
+	Matrix<int> fill_map{width, height, 0};
+
+	int color = 1;
+	for (int x = 0; x < fields.Width(); ++x) {
+		for (int y = 0; y < fields.Height(); ++y) {
+			if (fill_map.At(x, y) == 0) {
+				FloodFillTo(fill_map, fields, {x, y}, color++);
+			}
+		}
+	}
+	return fill_map;
 }
--- semi-final/src/FloodFill.h
+++ semi-final/src/FloodFill.h
@@ -3,5 +3,16 @@
 #include "Field.h"
 #include "Matrix.h"
 
-// return type is matrix of bool, but it's int for vector<bool> reasons
-Matrix<int> FloodFill(const Matrix<Field>& field, const Point& origin);
+// will contain 1 where origin is reachable (0 otherwise)
+Matrix<int> FloodFill(
+	const Matrix<Field>& fields,
+	const Point& origin);
+
+void FloodFillTo(
+	Matrix<int>& fill_matrix,
+	const Matrix<Field>& fields,
+	const Point& origin,
+	int fill_value = 1);
+
+// coordinates with the same integer value are reachable from each other
+Matrix<int> FullFloodFill(const Matrix<Field>& fields);
