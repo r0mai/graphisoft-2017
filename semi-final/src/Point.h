34af11a3d2ce261b83428710c815bddd45063473
Andras Kucsma <andras.kucsma@prezi.com>
2017-04-22 14:14:00 +0200

    Output invalid Point as (X, X) for alignment

--- semi-final/src/Point.h
+++ semi-final/src/Point.h
@@ -22,14 +22,19 @@ inline bool operator<(const Point& lhs, const Point& rhs) {
 	return std::tie(lhs.x, lhs.y) < std::tie(rhs.x, rhs.y);
 }
 
-inline std::ostream& operator<<(std::ostream& os, const Point& p) {
-	return os << '(' << p.x << ", " << p.y << ')';
-}
-
 inline bool IsValid(const Point& p) {
 	return p.x != -1 || p.y != -1;
 }
 
 inline int TaxicabDistance(const Point& a, const Point& b) {
 	return std::labs(a.x - b.x) + std::labs(a.y - b.y);
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Point& p) {
+	if (!IsValid(p)) {
+		os << "(X, X)";
+	} else {
+		os << '(' << p.x << ", " << p.y << ')';
+	}
+	return os;
 }
