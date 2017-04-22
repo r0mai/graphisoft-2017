9c9bce9c5d31fc7cd99f79f70e6f21aa0d5011e5
Matthias Vegh <matyas.vegh@gmail.com>
2017-04-22 15:19:54 +0200

    Add UpwindSailer to follow displays

---
+++ semi-final/src/UpwindSailer.cpp
@@ -0,0 +1,121 @@
+#include "UpwindSailer.h"
+
+#include <boost/optional.hpp>
+
+namespace {
+
+bool areWeAndTargetInSameRow(const Grid& grid, int player, int target) {
+	const auto& playerPosition = grid.Positions()[player];
+	const auto& displayPosition = grid.Displays()[target];
+	return playerPosition.y == displayPosition.y;
+}
+
+bool areWeAndTargetInSameColumn(const Grid& grid, int player, int target) {
+	const auto& playerPosition = grid.Positions()[player];
+	const auto& displayPosition = grid.Displays()[target];
+	return playerPosition.x == displayPosition.x;
+}
+
+using Direction = Point;
+
+boost::optional<Direction> inchCloser(const Grid& grid, int player, int target)
+{
+	if (areWeAndTargetInSameRow(grid, player, target)) {
+		return boost::none;
+	}
+	if (areWeAndTargetInSameColumn(grid, player, target)) {
+		return boost::none;
+	}
+
+	const auto& playerPosition = grid.Positions()[player];
+	const auto& displayPosition = grid.Displays()[target];
+
+	auto dx = displayPosition.x - playerPosition.x;
+	auto dy = displayPosition.y - playerPosition.y;
+
+	// Prefer large distances to arrive at the diagonal
+	if (dx <= dy) {
+		return Direction{dx>0?1:-1, 0};
+	} else {
+		return Direction{0, dy>0?1:-1};
+	}
+
+}
+
+Point getEdgeForRelativePush(const Grid& grid,
+		int player, const Direction& direction) {
+	const auto& position = grid.Positions()[player];
+	const auto& dx = direction.x;
+	const auto& dy = direction.y;
+	assert(std::abs(dx) <=1);
+	assert(std::abs(dy) <=1);
+	assert(dx * dy == 0);
+
+	auto x = 0;
+	auto y = 0;
+
+	switch(dx) {
+		case 0:
+			x = position.x;
+			break;
+		case -1:
+			x = grid.Width();
+			break;
+		case  1:
+			x = -1;
+			break;
+		default:
+			assert(false);
+	}
+
+	switch(dy) {
+		case 0:
+			y = position.y;
+			break;
+		case -1:
+			y = grid.Height();
+			break;
+		case  1:
+			y = -1;
+			break;
+		default:
+			assert(false);
+	}
+
+
+	return Point{x, y};
+}
+
+} // anonymous namespace
+
+void UpwindSailer::Init(int player) {
+	this->player = player;
+}
+
+void UpwindSailer::Shutdown() {
+}
+
+void UpwindSailer::Update(const Grid&, int) {
+}
+
+void UpwindSailer::Turn(
+		const Grid& grid, int player, int target, Field field, Callback fn) {
+	(void)player;
+	assert(player == this->player);
+	this->grid = grid;
+	this->target_display = target;
+	boost::optional<Direction> direction = inchCloser(grid, player, target);
+	if (direction) {
+		// TODO: See if we can move closer by using our GOTO
+		Response response{{{-1, -1}, field}, {-1, -1}};
+		response.push.edge =
+				getEdgeForRelativePush(grid, player, *direction);
+		fn(response);
+	} else {
+		// Already on same row/column as target
+		std::cerr << "On same row/column" << std::endl;
+	}
+}
+
+void UpwindSailer::Idle() {
+}
