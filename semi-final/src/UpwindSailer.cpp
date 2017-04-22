ef2d81adddfbabb15fc8e9bbbcd6f2b70626e8f8
Matthias Vegh <matyas.vegh@gmail.com>
2017-04-22 15:34:50 +0200

    Formatting fixes of UpwindSailer

--- semi-final/src/UpwindSailer.cpp
+++ semi-final/src/UpwindSailer.cpp
@@ -41,8 +41,7 @@ boost::optional<Direction> inchCloser(const Grid& grid, int player, int target)
 	}
 }
 
-boost::optional<Direction> evade(const Grid& grid, int player, int target)
-{
+boost::optional<Direction> evade(const Grid& grid, int player, int target) {
 	const auto& playerPosition = grid.Positions()[player];
 	if (areWeAndTargetInSameRow(grid, player, target)) {
 		return Direction{0, playerPosition.x == grid.Height() -1?-1:1};
@@ -58,8 +57,8 @@ Point getEdgeForRelativePush(const Grid& grid,
 	const auto& position = grid.Positions()[player];
 	const auto& dx = direction.x;
 	const auto& dy = direction.y;
-	assert(std::abs(dx) <=1);
-	assert(std::abs(dy) <=1);
+	assert(std::abs(dx) <= 1);
+	assert(std::abs(dy) <= 1);
 	assert(dx * dy == 0);
 
 	auto x = 0;
